d03e024f6570c447acb3b813f093c788b1abdfb1
barrialucas <lucasbarria742@gmail.com>
2022-04-19 21:33:15 -0300

    precios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import React, { useEffect } from "react";
 import "./css/style.css";
-import ReactGA from 'react-ga';
-
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-
 import {Navbar} from "./components/navfoo/Navbar"
 import {Footer} from "./components/navfoo/Footer"
 import {Home} from "./components/Home"
@@ -11,17 +7,8 @@ import {Camperas} from "./components/campera/Camperas"
 import {Sillas} from "./components/Sillas"
 import {ItemsContainer} from "./components/Lentes/ItemsContainer"
 
-import ReactGa from "react-ga"
-
-
 
 export default function App() {
-
-  useEffect(()=>{
-    React.initialize("G-GHZSR8ZQNL")
-
-    ReactGa.pageview("/")
-  },[])
 
   return (
     <BrowserRouter>
@@ -35,8 +22,6 @@ export default function App() {
         <Route path="/drift" element={<Sillas/>}/>
 
       </Routes>
-
-
 
       <Footer/>
     </BrowserRouter>
--- src/components/Lentes/Lentes.js
+++ src/components/Lentes/Lentes.js
@@ -2,14 +2,18 @@ import React from "react";
 
 export const Lentes =({id, marca,modelo,precio,color,img,hover})=>{
     
-    const mensaje="https://api.whatsapp.com/send?phone=542901580214&text=Hola%20deseo%20mas%20informaci%C3%B3n%20acerca%20del%20modelo" +modelo +"%20//" +color;
-
+    const mensaje="https://api.whatsapp.com/send?phone=542901580214&text=Hola%20deseo%20mas%20informaci%C3%B3n%20acerca%20del%20modelo" +modelo +"%20//" + color;
+    let precio15=(precio*85)/100
     return(
         <div className="card-body product-card3 col-xxl-4 productos-impresos cards p-3">
                                 <img src={img}  class="card-img-top img" alt={id}/>
                                 <h4 className="card-title d-flex justify-content-center titulo-card">{marca.toUpperCase()} {modelo}</h4>
                                 <h5 className="color d-flex justify-content-center">{color}</h5>
-                                <h5 className="precio card-text d-flex justify-content-center"><span className="precioHtml">${precio}</span></h5>
+                                <div className="d-flex justify-content-evenly precios">
+                                    <h5 className="precio card-text"><span className="precioHtml">${precio}</span></h5>
+                                    <h5 className="newPrice"><span className="off15">15%OFF </span>${precio15}</h5>
+                                </div>
+                                
                                 <div className="d-flex justify-content-center">
                                     <a class="card-btn d-flex justify-content-center" href={mensaje}>Contactarse</a>
                                 </div>
