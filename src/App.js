82e73f4706144de8a2350e29810cd230e4cfbe6d
barrialucas <lucasbarria742@gmail.com>
2022-05-30 23:01:47 -0300

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route,Navigate} from "react-router-dom";
 import {Navbar} from "./components/navfoo/Navbar"
 import {Footer} from "./components/navfoo/Footer"
 import {Home} from "./components/Home"
-import {Camperas} from "./components/campera/Camperas"
+
 import {Sillas} from "./components/Sillas"
 import {ItemsContainer} from "./components/Lentes/ItemsContainer"
 
@@ -18,7 +18,7 @@ export default function App() {
         <Route path="/" element={<Home/>}/>
 
         <Route path="/rayban" element={<ItemsContainer/>}/>
-        {/* <Route path="/northface" element={<Camperas/>}/> */}
+        
         <Route path="/drift" element={<Sillas/>}/>
 
         <Route path="*" element={ <Navigate to="/"/>} />
